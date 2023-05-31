c33f9179dc054da00cea81e6aae5747426c4f5d8
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-05-31 19:59:28 +0200

    Initial ClusterFuzzLite config for radixdlt-scrypto

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -eu
 
-# build project
-# e.g.
-# ./autogen.sh
-# ./configure
-# make -j$(nproc) all
+if [ -d $WORK/target ] ; then
+    cp -dprf $WORK/target .
+fi
+cargo +nightly fuzz build \
+    --release \
+    --no-default-features --features std,libfuzzer-sys \
+    --fuzz-dir . \
+    --no-cfg-fuzzing transaction
 
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     $LIB_FUZZING_ENGINE /path/to/library.a
+cp -dprf target $WORK
+find target -name transaction
+
+cp target/x86_64-unknown-linux-gnu/release/transaction $OUT/transaction_fuzzer
